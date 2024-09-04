fcca307b2e9dc5e25ec743129c9772fdb47964ca
saadat <devilwq99@gmail.com>
2024-09-04 18:38:47 +0600

    folder

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import Header from './component/header/Header'
 
 const App = () => {
+
+
+  
   return (
     <div>
+
       <Header/>
+
     </div>
   )
 }
---
+++ src/router/RoutApp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RoutApp = () => {
+  return (
+    <div>RoutApp</div>
+  )
+}
+
+export default RoutApp
