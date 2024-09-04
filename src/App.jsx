59e117e8c40e17fd7a9ebff4d31ad8c1cb8a2fdd
saadat <devilwq99@gmail.com>
2024-09-04 19:33:38 +0600

    post

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,79 @@
-import React from 'react'
-import Header from './component/header/Header'
+import React, { useEffect, useState } from 'react';
+import Header from './component/header/Header';
+import index from './api/index'; 
 
 const App = () => {
+  const [name, setName] = useState('');
+  const [surname, setSurname] = useState('');
+  const [email, setEmail] = useState('');
+  const [age, setAge] = useState('');
+  const [password, setPassword] = useState('');
 
+  const handlePost = async () => {
+    try {
+      const response = await index.fetchDress(name, surname, age, email, password);
+      console.log(response, '---------post---------------');
+    } catch (error) {
+      console.error('Error posting data:', error);
+    }
+  };
 
-  
+  useEffect(() => {
+    const fetchUse = async () => {
+      try {
+        const response = await index.fetchUser();
+        console.log(response, '----data------');
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+      }
+    };
+
+    const fetchDr = async () => {
+      try {
+        const response = await index.fetchDress(); 
+        console.log(response, '----data------');
+      } catch (error) {
+        console.error('Error fetching dress data:', error);
+      }
+    };
+
+    fetchUse();
+    fetchDr();
+  }, []); 
+
   return (
     <div>
-
-      <Header/>
-
+      <Header />
+      <div className="form">
+        <input
+          onChange={(e) => setName(e.target.value)}
+          type="text"
+          placeholder="name"
+        />
+        <input
+          onChange={(e) => setSurname(e.target.value)}
+          type="text"
+          placeholder="sur name"
+        />
+        <input
+          onChange={(e) => setAge(e.target.value)}
+          type="text"
+          placeholder="age"
+        />
+        <input
+          onChange={(e) => setEmail(e.target.value)}
+          type="text"
+          placeholder="email"
+        />
+        <input
+          onChange={(e) => setPassword(e.target.value)}
+          type="text"
+          placeholder="password"
+        />
+        <button onClick={handlePost}>Отправить</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { index } from '../api/index'
 
 const HomePage = () => {
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const response = await index.fetchUser()
+      console.log(response, '---data----');
+      
+    }
+    fetchUsers();
+
+
+  }, [])
   return (
     <div>Saadat</div>
   )
